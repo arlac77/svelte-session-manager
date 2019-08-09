c1e91956ad77adcde528277aac9420a2e28b061e
Markus Felten <Markus.Felten@gmx.de>
2019-08-09 15:13:29 +0200

    chore(package): correct repository url chore(package): set bugs url from template chore(package): homepage from template chore(package): set template repo chore(package): add chromedriver@^76.0.0 chore(package): add nightwatch@^1.1.13 chore(package): add rollup@^1.19.3 chore(package): add rollup-plugin-node-resolve@^5.2.0 chore(package): add rollup-plugin-svelte@^5.1.0 chore(package): add semantic-release@^15.13.19 chore(package): add serve@^11.1.0 chore(package): add svelte@^3.7.1 chore(scripts): add pretest@(cd example && rollup -c) chore(scripts): add start@npx serve -u -s -n -l 5000 example/public chore(scripts): add test@nightwatch -e ci chore(engines): add node@>=10.16.2 chore(package): add description from template chore(package): add version from template chore(package): add svelte from template chore(package): add module from template chore(package): add keywords from template chore(package): remove unknown value for description ({{description}}) chore(rollup): copy from template chore(license): add LICENSE chore(git): merge .gitignore with template chore(npm): merge .npmignore with template chore: add missing example/public/index.html from template chore: add missing example/src/App.svelte from template chore: add missing example/src/index.mjs from template

---
+++ example/rollup.config.js
@@ -0,0 +1,12 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "rollup-plugin-node-resolve";
+
+export default {
+  input: "src/index.mjs",
+  output: {
+    sourcemap: true,
+    format: "esm",
+    file: `public/bundle.mjs`
+  },
+  plugins: [resolve(), svelte()]
+};
