beb777729e048ec9529d0f574e078958fc2d13d6
Markus Felten <Markus.Felten@gmx.de>
2019-08-11 12:04:50 +0200

    chore(rollup): start server in watch mode

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,8 +1,22 @@
-import handler from "serve-handler";
-
-import http from "http";
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
+
+import http from "http";
+import handler from "serve-handler";
+
+const port = 5000;
+
+if (process.env.ROLLUP_WATCH) {
+  const server = http.createServer((request, response) => {
+    return handler(request, response, {
+      public: "public"
+    });
+  });
+
+  server.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`);
+  });
+}
 
 export default {
   input: "src/index.mjs",
