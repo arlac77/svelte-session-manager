5397b60d73930d4013927e61dde666bf752640ab
Markus Felten <Markus.Felten@gmx.de>
2019-12-27 20:45:49 +0100

    style: reformat

--- src/index.svelte
+++ src/index.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-  import {Session, login } from './index.mjs';
-  import Login from './Login.svelte';
+  import { Session, login } from "./index.mjs";
+  import Login from "./Login.svelte";
   export { Session, Login, login };
 </script>
