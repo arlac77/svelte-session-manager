b12d592ae15c2f17ee8e361a917da9471d257560
Markus Felten <Markus.Felten@gmx.de>
2020-11-11 03:53:20 +0100

    chore(netlify): import jsonwebtoken

--- tests/app/serverless/login.js
+++ tests/app/serverless/login.js
@@ -1,6 +1,9 @@
+const jsonwebtoken = require("jsonwebtoken");
+
+
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" })
+    body: JSON.stringify({ message: "Hello World", sign: jsonwebtoken.sign ? "sing present" : "no sign"})
   };
 };
