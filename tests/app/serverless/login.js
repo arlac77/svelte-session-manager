a0d1bd07e395f0c7e2ada819bf4e4bd68fbe2b36
Markus Felten <Markus.Felten@gmx.de>
2020-11-11 03:18:01 +0100

    chore(nelify): redirect api for .netlifly

--- tests/app/serverless/login.js
+++ tests/app/serverless/login.js
@@ -1,4 +1,4 @@
-export async function handler (event, context) {
+exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" })
