44df8c6b620b48deacd6f860019e41326abfc8ee
Markus Felten <Markus.Felten@gmx.de>
2020-11-11 12:09:30 +0100

    test: sign on netlify

--- tests/app/serverless/login.js
+++ tests/app/serverless/login.js
@@ -1,9 +1,20 @@
 const jsonwebtoken = require("jsonwebtoken");
-
+const { readFileSync } = require("fs");
 
 exports.handler = async (event, context) => {
+  const access_token = jsonwebtoken.sign(
+    { entitlements: ["a", "b", "c"].join(",") },
+    readFileSync(join(__dirname__, "demo.rsa")),
+    {
+      algorithm: "RS256",
+      expiresIn: "15s"
+    }
+  );
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World", sign: jsonwebtoken.sign ? "sing present" : "no sign"})
+    body: JSON.stringify({
+      access_token
+    })
   };
 };
