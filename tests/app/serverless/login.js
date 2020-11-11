6dba545afa429f10bb877d7655d6031fde6a8b96
Markus Felten <Markus.Felten@gmx.de>
2020-11-11 12:17:05 +0100

    test: add missing path

--- tests/app/serverless/login.js
+++ tests/app/serverless/login.js
@@ -1,5 +1,6 @@
-const jsonwebtoken = require("jsonwebtoken");
 const { readFileSync } = require("fs");
+const { join } = require("path");
+const jsonwebtoken = require("jsonwebtoken");
 
 exports.handler = async (event, context) => {
   const access_token = jsonwebtoken.sign(
