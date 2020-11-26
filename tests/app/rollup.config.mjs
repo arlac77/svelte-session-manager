d7de557d4dbb97d0f62a186493c805a16e85dfce
Markus Felten <Markus.Felten@gmx.de>
2020-11-26 18:32:53 +0100

    chore(rollup): import postcss chore(rollup): add postcss

--- tests/app/rollup.config.mjs
+++ tests/app/rollup.config.mjs
@@ -1,3 +1,5 @@
+import postcss from "rollup-plugin-postcss";
+
 import dev from "rollup-plugin-dev";
 import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
@@ -15,122 +17,117 @@ export default {
     format: "esm",
     file: `${basedir}/public/bundle.main.mjs`
   },
-  plugins: [
-    virtual({
-      "node-fetch": "export default fetch",
-      stream: "export class Readable {}",
-      buffer: "export class Buffer {}"
-    }),
-    svelte(),
-    resolve({
-      browser: true,
-      dedupe: importee =>
-        importee === "svelte" || importee.startsWith("svelte/")
-    }),
-    dev({
-      port,
-      dirs: [`${basedir}/public`],
-      spa: `${basedir}/public/index.html`,
-      basePath: "/",
-      extend(app, modules) {
-        app.use(
-          modules.router.post("/api/login", async (ctx, next) => {
-            const buffers = [];
+  plugins: [virtual({
+    "node-fetch": "export default fetch",
+    stream: "export class Readable {}",
+    buffer: "export class Buffer {}"
+  }), svelte(), resolve({
+    browser: true,
+    dedupe: importee =>
+      importee === "svelte" || importee.startsWith("svelte/")
+  }), dev({
+    port,
+    dirs: [`${basedir}/public`],
+    spa: `${basedir}/public/index.html`,
+    basePath: "/",
+    extend(app, modules) {
+      app.use(
+        modules.router.post("/api/login", async (ctx, next) => {
+          const buffers = [];
 
-            for await (const chunk of ctx.req) {
-              buffers.push(chunk);
-            }
+          for await (const chunk of ctx.req) {
+            buffers.push(chunk);
+          }
 
-            const content = JSON.parse(Buffer.concat(buffers).toString("utf8"));
-            if (
-              content.username.startsWith("user") &&
-              content.password === "secret"
-            ) {
-              const body = {
-                access_token: jsonwebtoken.sign(
-                  content.username.toLowerCase().includes("no_entitlements")
-                    ? {}
-                    : { entitlements: ["a", "b", "c"].join(",") },
-                  readFileSync("tests/app/demo.rsa"),
-                  {
-                    algorithm: "RS256",
-                    expiresIn: "8s"
-                  }
-                )
-              };
-
-              if (
-                !content.username.toLowerCase().includes("no_refresh_token")
-              ) {
-                body.refresh_token = jsonwebtoken.sign(
-                  {},
-                  readFileSync("tests/app/demo.rsa"),
-                  {
-                    algorithm: "RS256",
-                    expiresIn: "20s"
-                  }
-                );
-              }
-
-              await new Promise(resolve =>
-                setTimeout(
-                  resolve,
-                  content.username.toLowerCase().includes("slow") ? 2000 : 500
-                )
-              );
-
-              ctx.body = body;
-            } else {
-              function message(n) {
-                const messages = {
-                  400: "Bad Request",
-                  401: "Unauthorized",
-                  500: "Internal Server Error",
-                  502: "Bad Gateway"
-                };
-                return messages[n] ? messages[n] : "Unknown";
-              }
-              let status = 401;
-
-              const m = content.username.match(/^error\s*(\d+)(\s+([\w\-]+))?/);
-              if (m) {
-                status = parseInt(m[1]);
-
-                switch (m[3]) {
-                  case "json":
-                    ctx.type = "application/json";
-                    ctx.body = { key: "value" };
-                    break;
-                  case "html":
-                    ctx.type = "text/html";
-                    ctx.body = `<html><head><title>#HT ${message(
-                      status
-                    )}</title></head><body><center><h1>#H ${message(
-                      status
-                    )}</h1></center><center>nginx/1.17.4</center></body></html>`;
-                    break;
-                  case "WWW-Authenticate":
-                    ctx.set("WWW-Authenticate", 'Bearer realm="example"');
-                    ctx.append("WWW-Authenticate", 'error="invalid_token"');
-                    ctx.append(
-                      "WWW-Authenticate",
-                      `error_description="#W ${message(status)}"`
-                    );
-                    ctx.body = "WWW-Authenticate " + message(status);
-                    break;
-                  default:
-                    ctx.body = "#T " + message(status);
+          const content = JSON.parse(Buffer.concat(buffers).toString("utf8"));
+          if (
+            content.username.startsWith("user") &&
+            content.password === "secret"
+          ) {
+            const body = {
+              access_token: jsonwebtoken.sign(
+                content.username.toLowerCase().includes("no_entitlements")
+                  ? {}
+                  : { entitlements: ["a", "b", "c"].join(",") },
+                readFileSync("tests/app/demo.rsa"),
+                {
+                  algorithm: "RS256",
+                  expiresIn: "8s"
                 }
-              } else {
-                ctx.body = { message: message(status) };
-              }
-              ctx.status = status;
-              return;
+              )
+            };
+
+            if (
+              !content.username.toLowerCase().includes("no_refresh_token")
+            ) {
+              body.refresh_token = jsonwebtoken.sign(
+                {},
+                readFileSync("tests/app/demo.rsa"),
+                {
+                  algorithm: "RS256",
+                  expiresIn: "20s"
+                }
+              );
             }
-            next();
-          })
-        );
-      }
-    })
-  ]
+
+            await new Promise(resolve =>
+              setTimeout(
+                resolve,
+                content.username.toLowerCase().includes("slow") ? 2000 : 500
+              )
+            );
+
+            ctx.body = body;
+          } else {
+            function message(n) {
+              const messages = {
+                400: "Bad Request",
+                401: "Unauthorized",
+                500: "Internal Server Error",
+                502: "Bad Gateway"
+              };
+              return messages[n] ? messages[n] : "Unknown";
+            }
+            let status = 401;
+
+            const m = content.username.match(/^error\s*(\d+)(\s+([\w\-]+))?/);
+            if (m) {
+              status = parseInt(m[1]);
+
+              switch (m[3]) {
+                case "json":
+                  ctx.type = "application/json";
+                  ctx.body = { key: "value" };
+                  break;
+                case "html":
+                  ctx.type = "text/html";
+                  ctx.body = `<html><head><title>#HT ${message(
+                    status
+                  )}</title></head><body><center><h1>#H ${message(
+                    status
+                  )}</h1></center><center>nginx/1.17.4</center></body></html>`;
+                  break;
+                case "WWW-Authenticate":
+                  ctx.set("WWW-Authenticate", 'Bearer realm="example"');
+                  ctx.append("WWW-Authenticate", 'error="invalid_token"');
+                  ctx.append(
+                    "WWW-Authenticate",
+                    `error_description="#W ${message(status)}"`
+                  );
+                  ctx.body = "WWW-Authenticate " + message(status);
+                  break;
+                default:
+                  ctx.body = "#T " + message(status);
+              }
+            } else {
+              ctx.body = { message: message(status) };
+            }
+            ctx.status = status;
+            return;
+          }
+          next();
+        })
+      );
+    }
+  }), postcss()]
 };
