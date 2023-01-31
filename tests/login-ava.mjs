cf595a2e64fdb8c2890149a6a25e4c703cae10fd
Markus Felten <Markus.Felten@gmx.de>
2023-01-31 17:18:52 +0100

    fix: copy all keys from tokenmap into seesion

--- tests/login-ava.mjs
+++ tests/login-ava.mjs
@@ -7,20 +7,37 @@ globalThis.fetch = async function (url, options) {
   return {
     ok: true,
     json: () => {
-      return options.body.length > 20 ? { access_token: "aaaa" } : {};
+      return options.body.length > 30
+        ? { access_token: "aaa", refresh_token: "bbb" }
+        : {};
     }
   };
 };
 
 test("login missing access_token", async t => {
   const session = new Session();
-  const message = await login(session, "user", "");
+  const message = await login(session, "api", "", "");
   t.is(message, "missing access_token");
   t.false(session.isValid);
 });
 
-test("login invalid token", async t => {
-  const session = new Session();
-  const message = await login(session, "user", "secret");
-  t.false(session.isValid);
+test("login data present", async t => {
+  let data;
+  const message = await login(
+    {
+      update(d) {
+        data = d;
+      }
+    },
+    "api",
+    "user",
+    "secret"
+  );
+
+  t.deepEqual(data, {
+    access_token: "aaa",
+    refresh_token: "bbb",
+    username: "user",
+    endpoint: "api"
+  });
 });
