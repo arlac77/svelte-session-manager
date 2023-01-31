f07a94acb930e559c6843bc5cc60beab432ac070
Markus Felten <Markus.Felten@gmx.de>
2023-01-31 16:50:50 +0100

    test: login ...

--- tests/login-ava.mjs
+++ tests/login-ava.mjs
@@ -3,17 +3,25 @@ import { login } from "../src/login.mjs";
 import { Session } from "../src/session.mjs";
 
 globalThis.localStorage = {};
-globalThis.fetch = async function () {
+globalThis.fetch = async function (url, options) {
   return {
     ok: true,
     json: () => {
-      return {};
+      console.log(options.body.length);
+      return options.body.length > 20 ? { access_token: "aaaa" } : {};
     }
   };
 };
 
 test("login missing access_token", async t => {
   const session = new Session();
-  const message = await login(session, "user", "secret");
+  const message = await login(session, "user", "");
   t.is(message, "missing access_token");
+  t.false(session.isValid);
+});
+
+test("login invalid token", async t => {
+  const session = new Session();
+  const message = await login(session, "user", "secret");
+  t.false(session.isValid);
 });
