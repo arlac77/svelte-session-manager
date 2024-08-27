fbbfd7759277f7c6c596503187639aa10dc05ba5
Markus Felten <Markus.Felten@gmx.de>
2024-08-27 21:06:07 +0100

    fix: bind username & password

--- src/Login.svelte
+++ src/Login.svelte
@@ -2,32 +2,31 @@
   import { login } from "./login.mjs";
 
   let { endpoint, session, result,
-    inputsComponent=defaultInputsComponent,
     submitComponent=defaultSubmitComponent,
     footerComponent=defaultFooterComponent,
     messageComponent=defaultMessageComponent } = $props();
 
   let username = $state("");
   let password = $state("");
+  let enabled = $derived(username.length !== 0 && password.length !== 0);
   let active = $state(false);
   let message = $state();
 
   async function onsubmit(event) {
     event.preventDefault();
-    if (active) {
-      return;
-    }
-    try {
-      active = true;
-      message = await login(session, endpoint, username, password);
-      if (!message && result !== undefined) {
-        await result();
+    if (!active) {
+      try {
+        active = true;
+        message = await login(session, endpoint, username, password);
+        if (!message && result !== undefined) {
+          await result();
+        }
+      } catch (e) {
+        message = e;
+      } finally {
+        active = false;
+        password = "";
       }
-    } catch (e) {
-      message = e;
-    } finally {
-      active = false;
-      password = "";
     }
   }
 </script>
@@ -42,52 +41,11 @@
 {/if}
 {/snippet}
 
-{#snippet defaultInputsComponent(username,password,active)}
-<fieldset>
-  <label>
-    Username
-    <input
-      aria-label="username"
-      aria-required="true"
-      minlength="1"
-      maxlength="75"
-      size="32"
-      autocorrect="off"
-      autocapitalize="off"
-      autocomplete="username"
-      id="username"
-      type="text"
-      placeholder="Username"
-      required
-      disabled={active}
-    />
-  </label>
-  <label>
-    Password
-    <input
-      aria-label="password"
-      aria-required="true"
-      minlength="4"
-      maxlength="50"
-      size="32"
-      autocorrect="off"
-      autocapitalize="off"
-      autocomplete="current-password"
-      id="password"
-      type="password"
-      placeholder="Password"
-      required
-      disabled={active}
-    />
-  </label>
-</fieldset>
-{/snippet}
-
-{#snippet defaultSubmitComponent(username,password,active)}
+{#snippet defaultSubmitComponent(username,password,active,enabled)}
 <button
 aria-keyshortcuts="Enter"
 type="submit"
-disabled={!username || !password}
+disabled={!enabled}
 >
 Login
 {#if active}
@@ -98,7 +56,47 @@ Login
 
 <form {onsubmit}>
   {@render messageComponent(message)}
-  {@render inputsComponent(username,password,active)}
-  {@render submitComponent(username,password,active)}
+  <fieldset>
+    <label>
+      Username
+      <input
+        aria-label="username"
+        aria-required="true"
+        minlength="1"
+        maxlength="75"
+        size="32"
+        autocorrect="off"
+        autocapitalize="off"
+        autocomplete="username"
+        id="username"
+        type="text"
+        placeholder="Username"
+        required
+        disabled={active}
+        bind:value={username}
+      />
+    </label>
+    <label>
+      Password
+      <input
+        aria-label="password"
+        aria-required="true"
+        minlength="4"
+        maxlength="50"
+        size="32"
+        autocorrect="off"
+        autocapitalize="off"
+        autocomplete="current-password"
+        id="password"
+        type="password"
+        placeholder="Password"
+        required
+        disabled={active}
+        bind:value={password}
+      />
+    </label>
+  </fieldset>
+  
+  {@render submitComponent(username,password,active,enabled)}
   {@render footerComponent(username,password,active)}
 </form>
